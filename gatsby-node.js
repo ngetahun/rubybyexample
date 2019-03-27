150f65932024cfb9d95deeb81420eb37aff49f10
Natnael Getahun <connect@ngetahun.me>
2019-03-27 20:39:12 +0100

    Add example rendering

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,43 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    return new Promise((resolve, reject) => {
+        const examplePageTemplate = "";
+        resolve(
+            graphql(
+                `
+                {
+                    allMdx(limit: 1000) {
+                      edges {
+                        node {
+                          frontmatter {
+                            path
+                          }
+                        }
+                      }
+                    }
+                  }
+                `
+            )
+        ).then( result => { 
+            if (result.errors) {
+                reject(result.errors)
+            }
+  
+            // Create pages for each markdown file.
+            result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+                const path = node.frontmatter.path
+                createPage({
+                    path,
+                    component: blogPostTemplate,
+                    context: {
+                        path,
+                    },
+                })
+            })
+        })
+    })
+}
