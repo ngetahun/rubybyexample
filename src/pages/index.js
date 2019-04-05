87afb76116d01c3bb141a6e394241979d56afd4b
Natnael Getahun <connect@ngetahun.me>
2019-04-05 11:40:43 +0200

    :blue_book: Add list of blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,45 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { graphql } from 'gatsby'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />{" "}
-    <h1> Hi people </h1> <p> Welcome to your new Gatsby site. </p>{" "}
-    <p> Now go build something great. </p>{" "}
+export default ({data}) => {
+  console.log(data)
+  return (
+    <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    {/* <h1> Hi people </h1> <p> Welcome to your new Gatsby site. </p>
+    <p> Now go build something great. </p> */}
     <div
       style={{
         maxWidth: `300px`,
         marginBottom: `1.45rem`,
       }}
     >
-      <Image imgPath="ruby_lang.png" />
-    </div>{" "}
-    <Link to="/page-2/"> Go to page 2 </Link>{" "}
-  </Layout>
-)
+      <Image />
+    </div>
+ 
+    {console.log(data.allMdx)}
+    {data.allMdx.edges.map(({node}) => (
+        <Link to={node.frontmatter.path}> { node.frontmatter.title } </Link>
+      )
+    )}
 
-export default IndexPage
+  </Layout>
+  )
+}
+
+export const query = graphql `
+  query {
+    allMdx(limit: 1000) {
+      edges {
+        node {
+          frontmatter {
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
