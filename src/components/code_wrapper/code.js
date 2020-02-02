92046480bcee95505233c5e30cc45caf922d5075
Natnael Getahun <connect@ngetahun.me>
2020-02-02 18:26:41 +0000

    feat: add syntax highlighting to code snippet view

--- src/components/code_wrapper/code.js
+++ src/components/code_wrapper/code.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { CodeSnippet } from 'carbon-components-react';
 import styled from 'styled-components';
+import CodeBlock from './code_block';
 
 const codeSnippetProps = {
   type: 'multi',
@@ -9,11 +10,23 @@ const codeSnippetProps = {
   light: false,
 };
 
+const codeBlockProps = {
+  style: {},
+};
+
 const Container = styled.div``;
 
+const CodeEditor = props => {
+  return (
+    <CodeSnippet {...codeSnippetProps} {...props}>
+      <CodeBlock {...props} />
+    </CodeSnippet>
+  );
+};
 const components = {
-  pre: props => <CodeSnippet {...codeSnippetProps} {...props} />,
-  // code: props => <CodeSnippet {...codeSnippetProps} {...props} />,
+  //   pre: props => <CodeSnippet {...codeSnippetProps} {...props} />,
+  //   code: props => <CodeSnippet {...codeSnippetProps} {...props} />,
+  code: CodeEditor,
 };
 
 const CodeWrapper = props => {
